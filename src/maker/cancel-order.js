9e97b133613ffc2db7937af965e15ea2117b027e
Dan Pazuchowski <danpazuchowski@Dans-iMac.local>
2018-04-02 01:39:26 -0700

    refactor of a few methods and added cancel order

--- src/maker/cancel-order.js
+++ src/maker/cancel-order.js
@@ -7,16 +7,31 @@
  * @returns {Array<STATUS, Object>} response
  */
 
+const { status } = require('grpc');
+
 const { Order } = require('../models');
 
-async function cancelOrder(orderId) {
+async function cancelOrder(call, cb) {
+  const { makerId, orderId } = call.request;
+
+  this.logger.info('cancelOrder: attempting to cancel order', { orderId, makerId });
+
   // Get an order w/ a placed status
   // update the order as cancelled
   // emil cancelled event to all subscribers
   // return response
-  const order = new Order({ id: orderId });
+  const order = new Order();
+
   this.eventHandler.emit('order:cancelled', order);
-  return ['CANCELLED', order.export()];
+
+  const failedToCancel = false;
+
+  if (failedToCancel) {
+    this.logger.error('cancelOrder: failed to cancel order', { orderId, makerId });
+    return cb({ message: 'Invalid Order: Could not process', code: status.CANCELLED });
+  }
+
+  return cb(null, { ownerUuid: order.makerId, orderId: order.id });
 }
 
 module.exports = cancelOrder;
--- src/orderbook/watch.js
+++ src/orderbook/watch.js
@@ -10,6 +10,16 @@ async function watch(call) {
       fillAmount: new BigNumber(0).toFixed(0),
     });
   });
+
+  this.eventHandler.on('order:cancelled', (order) => {
+    call.write({
+      orderId: order.id,
+      baseSymbol: order.baseSymbol,
+      counterSymbol: order.counterSymbol,
+      status: order.status,
+      fillAmount: new BigNumber(0).toFixed(0),
+    });
+  });
 }
 
 module.exports = watch;
