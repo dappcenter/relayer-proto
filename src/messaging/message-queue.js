0739c16c0c4dbf7c035cb62b0978e5343f3cef08
Trey Griffith <trey.griffith@gmail.com>
2018-04-07 12:13:49 -0500

    wip

--- src/messaging/message-queue.js
+++ src/messaging/message-queue.js
@@ -7,17 +7,20 @@
  */
 
 const EventEmitter = require('events');
+const redis = require('redis');
+const client = 
 
 class MessageQueue extends EventEmitter {
   constructor() {
+    super();
     this._queue = [];
     this._listeners = [];
 
     this.on('enqueue', () => {
-      if(this._listeners.length) {
+      if (this._listeners.length) {
         const item = this.dequeue();
-        let listener
-        while(this._listeners.length) {
+        let listener;
+        while (this._listeners.length) {
           listener = this._listeners.shift();
           listener(item);
         }
@@ -38,11 +41,11 @@ class MessageQueue extends EventEmitter {
 
   // TODO: a way for listeners to take themselves out
   next() {
-    return new Promise((resolve, reject) => {
-      if(this._queue.length) {
+    return new Promise((resolve) => {
+      if (this._queue.length) {
         return resolve(this.dequeue());
       }
-      this._listeners.push(resolve);
+      return this._listeners.push(resolve);
     });
   }
 }
