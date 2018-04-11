ab187751b1097639d5c856049788a960c2bd925e
Dan Pazuchowski <danpazuchowski@Dans-iMac.local>
2018-04-11 12:44:50 -0700

    rename of variable

--- src/payment-engines/lnd-engine.js
+++ src/payment-engines/lnd-engine.js
@@ -69,9 +69,9 @@ class LndEngine {
   /**
    * Check's an invoice status
    */
-  async invoiceStatus(paymentRequest) {
+  async invoiceStatus(rHash) {
     return new Promise((resolve, reject) => {
-      this.client.lookupInvoice({ rHash: paymentRequest }, (err, res) => {
+      this.client.lookupInvoice({ rHash }, (err, res) => {
         if (err) return reject(err);
         return resolve(res);
       });
