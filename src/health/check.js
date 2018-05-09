f4146df8205a6f8acf8153d749c6d7d557a481df
Martine Ehrlich <martineehrlich@Martines-MacBook-Pro.local>
2018-05-09 11:50:28 -0400

    use new formatting for error handling

--- src/health/check.js
+++ src/health/check.js
@@ -1,5 +1,16 @@
-async function check (call, cb) {
-  cb(null, {status: 'SERVING'})
+const STATUS_OK = 'OK'
+/**
+ * Checks that the relayer is up and running
+ *
+ * @param {Object} req - request object
+ * @param {Grpc#Call} call - grpc call object
+ * @return {proto#HealthCheckResponse}
+ */
+async function check (req, call) {
+
+  return new this.proto.HealthCheckResponse({
+    status: STATUS_OK
+  })
 }
 
 module.exports = check
