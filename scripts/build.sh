6052fea13f2c53c04e8867133b2a77be99229e0d
Danny Paz <dan.pazuchowski@gmail.com>
2018-05-13 00:11:11 -0400

    FIX - Add lnd-engine docker files to relayer (#45)
    * updated orderbook proto
    
    * init commit of lnd-engine for relayer
    
    * added docker files to gitignore and added package lock file
    
    * downgrade docker-compose to version 2
    
    * install on build
    
    * engine update for package-lock
    
    * renamed kinesis folder to relayer and made context corrections for docker images for lnd
    
    * fixed rebuild for relayer

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,23 @@ echo ""
 echo "It's time to BUILD! All resistance is futile."
 echo ""
 
-# Rebuild gRPC to for docker target
-npm rebuild grpc --target_arch=x64 --target_platform=linux --target_libc=glibc
+npm i
+
+# Rebuilds gRPC to for docker target
 rm -f ./proto/relayer.proto
 npm run proto
+
+# Delete all files added from lnd-engine
+rm -rf ./docker/btcd
+rm -rf ./docker/lnd
+rm -f ./docker/docker-compose.yml
+rm -f ./docker/LND-README.md
+
+# Copy all docker files from lnd-engine and add them to our docker folder
+cp -a ./node_modules/lnd-engine/docker/. ./docker
+cp ./node_modules/lnd-engine/docker-compose.yml ./docker/lnd-docker-compose.yml
+
+# Rename the readme file for docker
+mv ./docker/README.md ./docker/LND-README.md
+
 npm test
