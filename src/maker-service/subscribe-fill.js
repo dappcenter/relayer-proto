1081767eaf452201559c05c619ae3ede22af6153
Trey Griffith <trey.griffith@gmail.com>
2018-06-14 00:08:41 -0700

    Feature: notify brokers of cancelled orders on subscribe (#68)
    * set up subscribe fill response for cancelled orders, and add a test skeleton
    
    * add test for stream-ending cancelled orders
    
    * update SubscribeFillResponse to include order status
    
    * use a special stream message instead of status
    
    * add docs
    
    * Feature: subscribe to fill notifications (Maker) (#69)
    
    * combine some validation
    
    * add tests for subscribeFill
    
    * update docs

--- src/maker-service/subscribe-fill.js
+++ src/maker-service/subscribe-fill.js
@@ -1,5 +1,5 @@
 /**
- * Given an order ID, open a stream for order exection commands
+ * Given an order ID, open a stream for order fill notificaitons
  *
  * @param {GrpcServerStreamingMethod~request} request - request object
  * @param {Object} request.params - Request parameters from the client
@@ -22,28 +22,40 @@ async function subscribeFill ({ params, send, logger, eventHandler, messenger },
     throw new Error(`No order with ID ${orderId}.`)
   }
 
+  /**
+   * Return only the order status if it is in a cancelled state and close the stream
+   */
+  if (order.status === Order.STATUSES.CANCELLED) {
+    send(new SubscribeFillResponse({
+      orderStatus: order.status
+    }))
+    return
+  }
+
   // TODO: ensure this user is authorized to connect to this order's stream
+
   if (order.status !== Order.STATUSES.PLACED) {
     throw new Error(`Cannot setup a fill listener for order in ${order.status} status.`)
   }
 
   // TODO: if they drop connection how do we make sure this listener doesn't get called
-  const fillId = await this.messenger.get(`fill:${order._id}`)
+  const fillId = await messenger.get(`fill:${order._id}`)
+
   const fill = await Fill.findOne({ fillId })
 
-  // TODO: how to handle this? Should we hide these from the client?
-  if (!fill) {
-    throw new Error('No fill found.')
+  // TODO: how to handle this? this is an error on the application side, not on the client side
+  if (!fill || fill.status !== Fill.STATUSES.ACCEPTED) {
+    throw new Error(`${fillId} is not a valid fill in the ${Fill.STATUSES.ACCEPTED} status.`)
   }
 
-  if (fill.status !== Fill.STATUSES.ACCEPTED) {
-    throw new Error('Only accepted status are valid fills.')
-  }
   await order.fill()
 
   send(new SubscribeFillResponse({
-    swapHash: fill.swapHash,
-    fillAmount: fill.fillAmount
+    orderStatus: order.status,
+    fill: {
+      swapHash: fill.swapHash,
+      fillAmount: fill.fillAmount
+    }
   }))
 
   // TODO: this should probably be sent after the message is sent to the client
---
+++ src/maker-service/subscribe-fill.spec.js
@@ -0,0 +1,153 @@
+const path = require('path')
+const { chai, rewire, sinon } = require('test/test-helper')
+const bigInt = require('big-integer')
+
+const { expect } = chai
+
+const subscribeFill = rewire(path.resolve(__dirname, 'subscribe-fill'))
+
+describe.only('subscribeFill', () => {
+  let params
+  let logger
+  let send
+  let eventHandler
+  let messenger
+  let SubscribeFillResponse
+  let metadata
+  let order
+  let fill
+  let fillStub
+  let orderStub
+  let fillOrderStub
+  let orderStatuses = {
+    CREATED: 'CREATED',
+    PLACED: 'PLACED',
+    CANCELLED: 'CANCELLED',
+    FILLED: 'FILLED',
+    COMPLETED: 'COMPLETED'
+  }
+  let fillStatuses = {
+    CREATED: 'CREATED',
+    ACCEPTED: 'ACCEPTED'
+  }
+  let fillId
+
+  beforeEach(() => {
+    logger = {
+      info: sinon.stub(),
+      error: sinon.stub()
+    }
+    send = sinon.stub()
+    eventHandler = {
+      emit: sinon.stub(),
+      on: sinon.stub()
+    }
+    SubscribeFillResponse = sinon.stub()
+
+    fillId = '3'
+
+    messenger = {
+      get: sinon.stub().resolves(fillId)
+    }
+    metadata = {}
+    fillOrderStub = sinon.stub()
+    order = { orderId: '2', _id: 'asfd', status: orderStatuses.PLACED, fill: fillOrderStub, payTo: 'ln:asdf1234', counterAmount: bigInt(1000), baseAmount: bigInt(100) }
+    fill = { fillId: '3', orderId: '2', order_id: 'asfd', status: fillStatuses.ACCEPTED, swapHash: '2309402394', fillAmount: '100' }
+
+    params = { orderId: '2' }
+
+    fillStub = {
+      findOne: sinon.stub().resolves(fill),
+      STATUSES: fillStatuses
+    }
+    subscribeFill.__set__('Fill', fillStub)
+
+    orderStub = {
+      findOne: sinon.stub().resolves(order),
+      STATUSES: orderStatuses
+    }
+    subscribeFill.__set__('Order', orderStub)
+  })
+
+  it('finds the order with the associated order id', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(orderStub.findOne).to.have.been.calledOnce()
+    expect(orderStub.findOne).to.have.been.calledWith(sinon.match.has('orderId', '2'))
+  })
+
+  it('throws an error if the order does not exist', () => {
+    orderStub.findOne.resolves()
+    return expect(subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })).to.eventually.rejectedWith('No order with ID')
+  })
+
+  it('sends the order status back alone if the order is cancelled', async () => {
+    order.status = orderStatuses.CANCELLED
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(send).to.have.been.calledOnce()
+    expect(SubscribeFillResponse).to.have.been.calledOnce()
+    expect(SubscribeFillResponse).to.have.been.calledWithNew()
+    expect(SubscribeFillResponse).to.have.been.calledWith({ orderStatus: orderStatuses.CANCELLED })
+    expect(send.args[0][0]).to.be.instanceOf(SubscribeFillResponse)
+  })
+
+  it('throws an error if the order is not in a placed state', () => {
+    order.status = orderStatuses.CREATED
+    return expect(subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })).to.eventually.rejectedWith('Cannot setup a fill listener for order in')
+  })
+
+  it('waits for the messenger to return', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(messenger.get).to.have.been.calledOnce()
+    expect(messenger.get).to.have.been.calledWith('fill:asfd')
+  })
+
+  it('finds the fill', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(fillStub.findOne).to.have.been.calledOnce()
+    expect(fillStub.findOne).to.have.been.calledWith(sinon.match.has('fillId', '3'))
+  })
+
+  it('throws if the fill does not exist', () => {
+    fillStub.findOne.resolves()
+    return expect(subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })).to.eventually.rejectedWith('is not a valid fill')
+  })
+
+  it('throws if the fill is in a bad state', () => {
+    fill.status = fillStatuses.CREATED
+    return expect(subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })).to.eventually.rejectedWith('is not a valid fill')
+  })
+
+  it('fills the order', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(fillOrderStub).to.have.been.calledOnce()
+    expect(fillOrderStub).to.have.been.calledOn(order)
+  })
+
+  it('sends the response to the client', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(send).to.have.been.calledOnce()
+    expect(SubscribeFillResponse).to.have.been.calledOnce()
+    expect(SubscribeFillResponse).to.have.been.calledWithNew()
+    expect(SubscribeFillResponse).to.have.been.calledWith({
+      orderStatus: orderStatuses.PLACED,
+      fill: {
+        swapHash: fill.swapHash,
+        fillAmount: fill.fillAmount
+      }
+    })
+    expect(send.args[0][0]).to.be.instanceOf(SubscribeFillResponse)
+  })
+
+  it('emits the fill event', async () => {
+    await subscribeFill({ params, send, logger, metadata, eventHandler, messenger }, { SubscribeFillResponse })
+
+    expect(eventHandler.emit).to.have.been.calledOnce()
+    expect(eventHandler.emit).to.have.been.calledWith('order:filled', order)
+  })
+})
